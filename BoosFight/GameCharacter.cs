7cc5fc21a0a6f2cab04e842ac110cae4dd12e881
Blazej002 <141903573+Blazej002@users.noreply.github.com>
2023-11-30 12:07:19 +0100

    Add project files.

---
+++ BoosFight/GameCharacter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoosFight
+{
+    internal class GameCharacter
+    {
+        public string Name;
+        public int HP;
+        public int Stamina;
+        public int StaminaMax;
+        public int Dmg;
+
+        public GameCharacter(string name, int hp, int stamina, int dmg)
+        {
+            Name = name;
+            HP = hp;
+            Stamina = stamina;
+            Dmg = dmg;
+            StaminaMax = stamina;
+        }
+
+        public int Attack()
+        {
+            if (Stamina <= 0) return -1; 
+            
+            var random = new Random();
+            var ran = random.Next(0, Dmg);
+           
+            var dmgDealt = Name == "Hero" ? Dmg : ran;
+            Stamina -= 10;
+            return dmgDealt;
+        }
+
+        public void Recharge()
+        {
+            Stamina = StaminaMax;
+        }
+    }
+}
---
+++ BoosFight/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace BoosFight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hero = new GameCharacter("Hero", 100, 40, 20);
+            var boss = new GameCharacter("Boss", 400, 10, 30);
+
+            bool winCond = false;
+            bool loseCond = false;
+            int dmg;
+            int dmgBoss;
+            bool noobieSave = true;
+            do
+            {
+                Console.WriteLine($"Hp: {hero.HP}    Stamina: {hero.Stamina}   Strengh: {hero.Dmg}");
+
+                Console.WriteLine();
+                //Player Actions
+                Console.WriteLine("1. Attack   2. Recharge");
+                var userInp = Console.ReadLine();
+
+                if (userInp == "1")
+                {
+                    dmg = hero.Attack();
+                    if (dmg == -1 && noobieSave)
+                    {
+                        //failsafe
+                        Console.WriteLine("You are out of stamina");
+                        Console.WriteLine("Automatic refil");
+                        hero.Stamina = hero.StaminaMax;
+                        noobieSave = false;
+                    }
+                    else if (dmg == -1 && !noobieSave)
+                    {
+                        dmg = 0;
+                        Console.WriteLine($"You've dealt {dmg}dmg to the boss! Boss has {boss.HP}hp left!!");
+                    }
+                    else
+                    {
+                        boss.HP -= dmg;
+                        Console.WriteLine($"You've dealt {dmg}dmg to the boss! Boss has {boss.HP}hp left!!");
+                    }
+                }
+                else if (userInp == "2")
+                {
+                    if (hero.Stamina == hero.StaminaMax)
+                    {
+                        Console.WriteLine("Congrats, you have just wasted a turn, your stamina was already max :'");
+                    }
+                    else
+                    {
+                        hero.Stamina = hero.StaminaMax;
+                        Console.WriteLine("You feel well rested....");
+                    }
+                }
+                //
+                //Boss from here on
+
+                if (boss.HP <= 0) winCond = true;
+                else
+                {
+                    Thread.Sleep(1000);
+                    if (boss.Stamina == 0)
+                    {
+                        boss.Recharge();
+                        Console.WriteLine("Boss rests...");
+                    }
+                    else
+                    {
+                        dmgBoss = boss.Attack();
+                        hero.HP -= dmgBoss;
+                        Console.WriteLine("Boss attacks you for {0}, you have {1}Hp left...", dmgBoss, hero.HP);
+                    }
+                }
+
+                if (hero.HP <= 0) loseCond = true;
+                Thread.Sleep(2000);
+                Console.Clear();
+            } while (!winCond && !loseCond);
+
+            if (winCond)
+            {
+                Console.WriteLine("Congrats you won!");
+            }
+            else Console.WriteLine("Game over");
+        }
+    }
+}
